381388b409564eefd6016c0f7c1b36f13a1143a5
ndowahcollins <105953900+ndowahcollins@users.noreply.github.com>
2022-08-04 22:40:05 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,8 +3,8 @@
     bucket = "code-sharing-bucket"
     key    = "dac.tfstate"
     region = "us-east-1"
-    profile = "jespo"
-dynamodb_table= "firstprojecttable"
+    profile = "ndowahcollins"
+dynamodb_table= "terraform-dynamo-locks"
    }
  }
 
